46fc6d536de50e284459b0170b144750e7b7b137
Vitor Melo <vitormelo9913@gmail.com>
2023-07-21 16:59:50 -0300

    criando Movie

---
+++ src/main/java/com/api/movies/entities/movie/Movie.java
@@ -0,0 +1,30 @@
+package com.api.movies.entities.movie;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "name", columnDefinition = "varchar(255)")
+    private String name;
+
+    @NotNull
+    @Column(name = "releaseDate", columnDefinition = "date")
+    private LocalDate releaseDate;
+
+    @Column(name = "rating", columnDefinition = "int")
+    private double rating;
+}
